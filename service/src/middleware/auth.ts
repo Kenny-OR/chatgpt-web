2f947af652ab0b536d396fe227fa5747c6a41113
Kenny <chunpangor@gmail.com>
2023-04-17 20:57:17 +0800

    fix: 第一版

--- service/src/middleware/auth.ts
+++ service/src/middleware/auth.ts
@@ -2,20 +2,35 @@ import {getSecretKeyList, isNotEmptyString} from '../utils/is'
 
 const auth = async (req, res, next) => {
     try {
-      const Authorization = req.header('Authorization')
-			// const AUTH_SECRET_KEY = process.env.AUTH_SECRET_KEY
-			const AUTH_SECRET_KEY = getSecretKeyList();
-			let hasAuth = false;
-			for (const key of AUTH_SECRET_KEY) {
-				if (Authorization && Authorization.replace('Bearer ', '').trim() === key) {
-					hasAuth = true;
-					break;
+      // const Authorization = req.header('Authorization')
+			// // const AUTH_SECRET_KEY = process.env.AUTH_SECRET_KEY
+			// const AUTH_SECRET_KEY = getSecretKeyList();
+			// let hasAuth = false;
+			// for (const key of AUTH_SECRET_KEY) {
+			// 	if (Authorization && Authorization.replace('Bearer ', '').trim() === key) {
+			// 		hasAuth = true;
+			// 		break;
+			// 	}
+			// }
+			//
+      // if (!hasAuth)
+      //   throw new Error('Error: 无访问权限 | No access rights')
+      // next()
+			const AUTH_SECRET_KEY = process.env.AUTH_SECRET_KEY
+			if (isNotEmptyString(AUTH_SECRET_KEY)) {
+				try {
+					const Authorization = req.header('Authorization')
+					if (!Authorization || Authorization.replace('Bearer ', '').trim() !== AUTH_SECRET_KEY.trim())
+						throw new Error('Error: 无访问权限 | No access rights')
+					next()
+				}
+				catch (error) {
+					res.send({ status: 'Unauthorized', message: error.message ?? 'Please authenticate.', data: null })
 				}
 			}
-
-      if (!hasAuth)
-        throw new Error('Error: 无访问权限 | No access rights')
-      next()
+			else {
+				next()
+			}
     }
     catch (error) {
       res.send({ status: 'Unauthorized', message: error.message ?? 'Please authenticate.', data: null })
